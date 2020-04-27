8a796def9ef6dfd84476992d5a71d4c50ab8eeab
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-04-27 17:03:04 +0530

    Dilanga servlet updated

--- LMS/src/com/lms/servlet/GetPayametServlet.java
+++ LMS/src/com/lms/servlet/GetPayametServlet.java
@@ -27,7 +27,7 @@ public class GetPayametServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
