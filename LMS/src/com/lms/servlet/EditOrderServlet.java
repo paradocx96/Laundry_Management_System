430943c1cd4e34bae5895fb203a80f242c817fe3
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-05-12 21:54:35 +0530

    Update EditOrderServlet.java

--- LMS/src/com/lms/servlet/EditOrderServlet.java
+++ LMS/src/com/lms/servlet/EditOrderServlet.java
@@ -2,18 +2,17 @@ package com.lms.servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.lms.model.Order;
-<<<<<<< HEAD
-import com.lms.service.OrderService;
-=======
 import com.lms.util.OrderDBUtil;
->>>>>>> branch 'master' of https://github.com/paradocx96/OOP-Project.git
 
 
 @WebServlet("/EditOrderServlet")
